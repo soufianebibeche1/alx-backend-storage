91a55cbb985c5032c8429a11c2c8eebb6d571f94
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-05-08 23:04:43 +0000

    add task 0

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,7 @@
--- Rank country origins of bands by the number of (non-unique) fans
-SELECT origin, COUNT(*) AS nb_fans
-FROM bands
+-- 2-fans.sql
+-- Ranks country origins of bands, ordered by the number of (non-unique) fans
+
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
